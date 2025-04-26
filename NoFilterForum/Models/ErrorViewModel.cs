983055cc1d25acaf4396b94bd8371ef691babf8b
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-26 08:40:34 +0300

    Add project files.

---
+++ NoFilterForum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NoFilterForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
