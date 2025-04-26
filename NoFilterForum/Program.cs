8329779f7f9d53b42bdd75a3e83fac9a287abd33
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-26 08:55:17 +0300

    chore: Changed IdentityUser to UserDataModel for db

--- NoFilterForum/Program.cs
+++ NoFilterForum/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using NoFilterForum.Data;
+using NoFilterForum.Models;
 
 namespace NoFilterForum
 {
@@ -16,7 +17,7 @@ namespace NoFilterForum
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<UserDataModel>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
