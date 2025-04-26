2a240bea5bc01d56612a0f3f7cd393eba41e3477
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-26 08:53:10 +0300

    feat: Added UserDataModel
    Added custom UserDataModel

---
+++ NoFilterForum/Models/UserDataModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace NoFilterForum.Models
+{
+    public class UserDataModel : IdentityUser
+    {
+        [Key]
+        [Required]
+        public string Id { get; private init; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public int Posts { get; set; }
+        public string Role { get; private init; }
+        public short Warnings { get; set; }
+    }
+}
