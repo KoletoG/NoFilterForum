1a36ec5243880b4aa2d7664fc0d4e91cdb0b36cc
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-04 10:42:01 +0300

    feat: Added IndexViewModel
    Added a property bool IsAdmin to use it for better checking in the index method validations.

---
+++ NoFilterForum/Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace NoFilterForum.Models.ViewModels
+{
+    public class IndexViewModel
+    {
+        public List<SectionDataModel> Sections { get; set; }
+        public bool IsAdmin { get; set; }
+        public IndexViewModel(List<SectionDataModel> sections,bool isAdmin) 
+        {
+            Sections = sections;
+            IsAdmin = isAdmin;
+        }
+    }
+}
