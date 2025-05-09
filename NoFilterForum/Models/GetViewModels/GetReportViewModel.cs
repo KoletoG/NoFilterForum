d3d9021815173dfcb77cbfd3be03d2b579ea409c
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-09 09:49:26 +0300

    chore: Added error-validation for reports

--- NoFilterForum/Models/GetViewModels/GetReportViewModel.cs
+++ NoFilterForum/Models/GetViewModels/GetReportViewModel.cs
@@ -1,13 +1,20 @@
-﻿using NoFilterForum.Models.DataModels;
+﻿using System.ComponentModel.DataAnnotations;
+using NoFilterForum.Models.DataModels;
 
 namespace NoFilterForum.Models.GetViewModels
 {
     public class GetReportViewModel
     {
+        [Required]
         public string UserIdTo { get; set; }
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Report message cannot be empty.")]
+        [MinLength(15, ErrorMessage ="Report message length should be at least 15 characters.")]
+        [DataType(DataType.MultilineText)]
         public string Content { get; set; }
+        [Required]
         public string IdOfPostReply { get; set; }
         public bool IsPost { get; set; }
+        [Required]
         public string Title { get; set; }
         public GetReportViewModel() 
         {
