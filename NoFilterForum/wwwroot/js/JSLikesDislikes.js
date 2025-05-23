a8bc14dd46c4d0a0f3c1baa39c6a716a612ae072
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-23 10:02:00 +0300

    feat: Users can now see updated likes and dislikes of replies

--- NoFilterForum/wwwroot/js/JSLikesDislikes.js
+++ NoFilterForum/wwwroot/js/JSLikesDislikes.js
@@ -42,3 +42,45 @@ function updatePostDislikes()
         likesCount.innerText=parseInt(likesCount.innerText)-1;
     }
 }
+function updateLikes(id)
+{
+    let btnLike = document.getElementById(`like_${id}`);
+    let likesCount = document.getElementById(`likesCount_${id}`);
+    let btnDislike = document.getElementById(`dislike_${id}`);
+    if(btnDislike.style.color==updatedColor)
+    {
+        btnDislike.style.color=normalColor;
+        likesCount.innerText=parseInt(likesCount.innerText)+1;
+    }
+    if(btnLike.style.color==updatedColor)
+    {
+        btnLike.style.color=normalColor;
+        likesCount.innerText=parseInt(likesCount.innerText)-1;
+    }
+    else
+    {
+        btnLike.style.color=updatedColor;
+        likesCount.innerText=parseInt(likesCount.innerText)+1;
+    }
+}
+function updateDislikes(id)
+{
+    let btnLike = document.getElementById(`like_${id}`);
+    let likesCount = document.getElementById(`likesCount_${id}`);
+    let btnDislike = document.getElementById(`dislike_${id}`);
+    if(btnLike.style.color==updatedColor)
+    {
+        btnLike.style.color=normalColor;
+        likesCount.innerText=parseInt(likesCount.innerText)-1;
+    }
+    if(btnDislike.style.color==updatedColor)
+    {
+        btnDislike.style.color=normalColor;
+        likesCount.innerText=parseInt(likesCount.innerText)+1;
+    }
+    else
+    {
+        btnDislike.style.color=updatedColor;
+        likesCount.innerText=parseInt(likesCount.innerText)-1;
+    }
+}
