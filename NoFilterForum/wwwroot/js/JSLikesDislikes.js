bcbeaa740fabeede5c439fcbcce1c9e78b11779b
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-23 08:45:53 +0300

    chore: Implemented JS script into PostView.cshtml

--- NoFilterForum/wwwroot/js/JSLikesDislikes.js
+++ NoFilterForum/wwwroot/js/JSLikesDislikes.js
@@ -1,1 +1,17 @@
-﻿
+﻿let likedPosts = new Set();
+let normalColor = "black";
+let updatedColor = "red";
+function postLike(btn)
+{
+    let btnLike = document.getElementById("postLike");
+    if(likedPosts.has(btn.id))
+    {
+        btnLike.style.color=normalColor;
+        likedPosts.delete(btn.id);
+    }
+    else
+    {
+        btnLike.style.color=updatedColor;
+        likedPosts.add(btnLike.id);
+    }
+}
