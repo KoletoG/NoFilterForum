bcdd31057e24686a8103e72c8dd39cba229340d2
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-23 08:50:48 +0300

    fix: Fixed pages not loading

--- NoFilterForum/wwwroot/js/JSLikesDislikes.js
+++ NoFilterForum/wwwroot/js/JSLikesDislikes.js
@@ -1,17 +1,20 @@
 ﻿let likedPosts = new Set();
 let normalColor = "black";
 let updatedColor = "red";
-function postLike(btn)
+function updateLikes(btn)
 {
     let btnLike = document.getElementById("postLike");
+    let likesCount = document.getElementById("likesCount");
     if(likedPosts.has(btn.id))
     {
         btnLike.style.color=normalColor;
         likedPosts.delete(btn.id);
+        likesCount.innerText=parseInt(likesCount.innerText)-1;
     }
     else
     {
         btnLike.style.color=updatedColor;
         likedPosts.add(btnLike.id);
+        likesCount.innerText=parseInt(likesCount.innerText)+1;
     }
 }
