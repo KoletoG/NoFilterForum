497ce92cf09423bd10b8b8cf3483ea724bdaf1bd
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-24 09:53:23 +0300

    feat: Added all fetching methods in WarningRepository
    Added them to IWarningRepository too

--- NoFilterForum/Repositories/Implementations/WarningRepository.cs
+++ NoFilterForum/Repositories/Implementations/WarningRepository.cs
@@ -1,8 +1,49 @@
-﻿using NoFilterForum.Repositories.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using NoFilterForum.Data;
+using NoFilterForum.Models.DataModels;
+using NoFilterForum.Repositories.Interfaces;
 
 namespace NoFilterForum.Repositories.Implementations
 {
     public class WarningRepository : IWarningRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public WarningRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<WarningDataModel> GetByIdAsync(string id)
+        {
+            return await _context.WarningDataModels.FindAsync(id);
+        }
+        public async Task<List<WarningDataModel>> GetAllByUserAsync(UserDataModel user)
+        {
+            return await _context.WarningDataModels.Where(x=>x.User==user).ToListAsync();
+        }
+        public async Task<List<WarningDataModel>> GetAllAsync()
+        {
+            return await _context.WarningDataModels.ToListAsync();
+        }
+        public async Task<WarningDataModel> CreateAsync(WarningDataModel warning)
+        {
+            await _context.WarningDataModels.AddAsync(warning);
+            await _context.SaveChangesAsync();
+            return warning;
+        }
+        public async Task UpdateAsync(WarningDataModel warning)
+        {
+            _context.WarningDataModels.Update(warning);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(WarningDataModel warning)
+        {
+            _context.WarningDataModels.Remove(warning);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<bool> ExistsByUserAsync(UserDataModel user)
+        {
+            return await _context.WarningDataModels.AnyAsync(x => x.User==user);
+        }
     }
 }
--- NoFilterForum/Repositories/Interfaces/IWarningRepository.cs
+++ NoFilterForum/Repositories/Interfaces/IWarningRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace NoFilterForum.Repositories.Interfaces
+﻿using NoFilterForum.Models.DataModels;
+
+namespace NoFilterForum.Repositories.Interfaces
 {
     public interface IWarningRepository
     {
+        public Task<WarningDataModel> GetByIdAsync(string id);
+        public Task<List<WarningDataModel>> GetAllByUserAsync(UserDataModel user);
+        public Task<List<WarningDataModel>> GetAllAsync();
+        public Task<WarningDataModel> CreateAsync(WarningDataModel warning);
+        public Task UpdateAsync(WarningDataModel warning);
+        public Task DeleteAsync(WarningDataModel warning);
+        public Task<bool> ExistsByUserAsync(UserDataModel user);
     }
 }
