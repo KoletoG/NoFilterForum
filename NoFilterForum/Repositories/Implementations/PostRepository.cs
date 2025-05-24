dd4be5472e002ac8f36a7a76d09e507f5780676c
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-24 18:39:42 +0300

    feat: Added methods to Post Repository

--- NoFilterForum/Repositories/Implementations/PostRepository.cs
+++ NoFilterForum/Repositories/Implementations/PostRepository.cs
@@ -1,8 +1,50 @@
-﻿using NoFilterForum.Repositories.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using NoFilterForum.Data;
+using NoFilterForum.Models.DataModels;
+using NoFilterForum.Repositories.Interfaces;
 
 namespace NoFilterForum.Repositories.Implementations
 {
     public class PostRepository : IPostRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public PostRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<PostDataModel> GetByIdAsync(string id)
+        {
+            return await _context.PostDataModels.FindAsync(id);
+        }
+        public async Task<List<PostDataModel>> GetAllByUserAsync(UserDataModel user)
+        {
+            return await _context.PostDataModels.Where(x => x.User == user).ToListAsync();
+        }
+        public async Task<List<PostDataModel>> GetAllAsync()
+        {
+            return await _context.PostDataModels.ToListAsync();
+        }
+        public async Task<PostDataModel> CreateAsync(PostDataModel post)
+        {
+            await _context.PostDataModels.AddAsync(post);
+            await _context.SaveChangesAsync();
+            return post;
+        }
+        public async Task<int> GetCountByPostIdAsync(string id)
+        {
+            return await _context.PostDataModels.Where(x => x.Id == id).CountAsync();
+        }
+        public async Task UpdateAsync(PostDataModel post)
+        {
+            _context.PostDataModels.Update(post);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(PostDataModel post)
+        {
+            _context.PostDataModels.Remove(post);
+            await _context.SaveChangesAsync();
+        }
     }
 }
--- NoFilterForum/Repositories/Interfaces/IPostRepository.cs
+++ NoFilterForum/Repositories/Interfaces/IPostRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace NoFilterForum.Repositories.Interfaces
+﻿using Microsoft.EntityFrameworkCore;
+using NoFilterForum.Models.DataModels;
+
+namespace NoFilterForum.Repositories.Interfaces
 {
     public interface IPostRepository
     {
+        public Task<PostDataModel> GetByIdAsync(string id);
+        public Task<List<PostDataModel>> GetAllByUserAsync(UserDataModel user);
+        public Task<List<PostDataModel>> GetAllAsync();
+        public Task<PostDataModel> CreateAsync(PostDataModel post);
+        public Task<int> GetCountByPostIdAsync(string id);
+        public Task UpdateAsync(PostDataModel post);
+        public Task DeleteAsync(PostDataModel post);
     }
 }
