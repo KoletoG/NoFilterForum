80780e2a3f333af644a1e92923e33998d2844a52
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-24 09:29:04 +0300

    chore: Added GET methods in UserRepository

--- NoFilterForum/Repositories/Implementations/UserRepository.cs
+++ NoFilterForum/Repositories/Implementations/UserRepository.cs
@@ -1,8 +1,29 @@
-﻿using NoFilterForum.Repositories.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using NoFilterForum.Data;
+using NoFilterForum.Models.DataModels;
+using NoFilterForum.Repositories.Interfaces;
 
 namespace NoFilterForum.Repositories.Implementations
 {
     public class UserRepository : IUserRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<UserDataModel> GetByIdAsync(string id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+        public async Task<UserDataModel> GetByUsernameAsync(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(x => x.UserName == username);
+        }
+        public async Task<List<UserDataModel>> GetAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
     }
 }
--- NoFilterForum/Repositories/Interfaces/IUserRepository.cs
+++ NoFilterForum/Repositories/Interfaces/IUserRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace NoFilterForum.Repositories.Interfaces
+﻿using NoFilterForum.Models.DataModels;
+
+namespace NoFilterForum.Repositories.Interfaces
 {
     public interface IUserRepository
     {
+        public Task<UserDataModel> GetByIdAsync(string id);
+        public Task<UserDataModel> GetByUsernameAsync(string username);
+        public Task<List<UserDataModel>> GetAllAsync();
     }
 }
