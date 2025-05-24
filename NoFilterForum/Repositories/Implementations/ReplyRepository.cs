fc731cfc7f4199f2dc9dcf45058dfb0b18f500f5
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-24 10:22:32 +0300

    feat: Added methods to Reply repository

--- NoFilterForum/Repositories/Implementations/ReplyRepository.cs
+++ NoFilterForum/Repositories/Implementations/ReplyRepository.cs
@@ -1,8 +1,49 @@
-﻿using NoFilterForum.Repositories.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using NoFilterForum.Data;
+using NoFilterForum.Models.DataModels;
+using NoFilterForum.Repositories.Interfaces;
 
 namespace NoFilterForum.Repositories.Implementations
 {
     public class ReplyRepository : IReplyRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public ReplyRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<ReplyDataModel> GetByIdAsync(string id)
+        {
+            return await _context.ReplyDataModels.FindAsync(id);
+        }
+        public async Task<List<ReplyDataModel>> GetAllByPostAsync(PostDataModel post)
+        {
+            return await _context.ReplyDataModels.Where(x => x.Post == post).ToListAsync();
+        }
+        public async Task<List<ReplyDataModel>> GetAllByUserAsync(UserDataModel user)
+        {
+            return await _context.ReplyDataModels.Where(x => x.User == user).ToListAsync();
+        }
+        public async Task<List<ReplyDataModel>> GetAllAsync()
+        {
+            return await _context.ReplyDataModels.ToListAsync();
+        }
+        public async Task<ReplyDataModel> CreateAsync(ReplyDataModel reply)
+        {
+            await _context.ReplyDataModels.AddAsync(reply);
+            await _context.SaveChangesAsync();
+            return reply;
+        }
+        public async Task UpdateAsync(ReplyDataModel reply)
+        {
+            _context.ReplyDataModels.Update(reply);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(ReplyDataModel reply)
+        {
+            _context.ReplyDataModels.Remove(reply);
+            await _context.SaveChangesAsync();
+        }
     }
 }
--- NoFilterForum/Repositories/Interfaces/IReplyRepository.cs
+++ NoFilterForum/Repositories/Interfaces/IReplyRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace NoFilterForum.Repositories.Interfaces
+﻿using Microsoft.EntityFrameworkCore;
+using NoFilterForum.Models.DataModels;
+
+namespace NoFilterForum.Repositories.Interfaces
 {
     public interface IReplyRepository
     {
+        public Task<ReplyDataModel> GetByIdAsync(string id);
+        public Task<List<ReplyDataModel>> GetAllByPostAsync(PostDataModel post);
+        public Task<List<ReplyDataModel>> GetAllByUserAsync(UserDataModel user);
+        public Task<List<ReplyDataModel>> GetAllAsync();
+        public Task<ReplyDataModel> CreateAsync(ReplyDataModel reply);
+        public Task UpdateAsync(ReplyDataModel reply);
+        public Task DeleteAsync(ReplyDataModel reply);
     }
 }
