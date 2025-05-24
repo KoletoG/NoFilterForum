185e91587b0325b8ce422baac9732f70ea548dfe
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-24 10:05:36 +0300

    feat: Added methods in SectionRepository

--- NoFilterForum/Repositories/Interfaces/ISectionRepository.cs
+++ NoFilterForum/Repositories/Interfaces/ISectionRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace NoFilterForum.Repositories.Interfaces
+﻿using Microsoft.EntityFrameworkCore;
+using NoFilterForum.Models.DataModels;
+
+namespace NoFilterForum.Repositories.Interfaces
 {
     public interface ISectionRepository
     {
+        public Task<SectionDataModel> GetByIdAsync(string id);
+        public Task<SectionDataModel> GetByTitleAsync(string title);
+        public Task<List<SectionDataModel>> GetAllAsync();
+        public Task<SectionDataModel> CreateAsync(SectionDataModel section);
+        public Task UpdateAsync(SectionDataModel section);
+        public Task DeleteAsync(SectionDataModel section);
+        public Task<bool> ExistsByTitleAsync(string title);
     }
 }
