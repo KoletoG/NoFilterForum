a085bfbdbefc14272df2bfc232b5d62ae3f051a6
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-25 20:21:01 +0300

    fix: Fixed bugs

---
+++ Core/Global variables/GlobalVariables.cs
@@ -0,0 +1,16 @@
+﻿using NoFilterForum.Core.Models.DataModels;
+
+namespace NoFilterForum.Global_variables
+{
+    public static class GlobalVariables
+    {
+        public static HashSet<string> adminNames = new HashSet<string>() {"Admin"};
+        public static UserDataModel DefaultUser = new UserDataModel() 
+        {
+            Id=Guid.NewGuid().ToString(),
+            Email="null@email.c",
+            UserName="default",
+            Role = UserRoles.Deleted
+        };
+    }
+}
