c53fdb082425b0cc490aa236e02ac0d30c15d21b
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-25 20:08:03 +0300

    feat: Added Infrastructure Project

---
+++ Infrastructure/Data/Migrations/20250505065325_UpdatedReplyDataModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NoFilterForum.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedReplyDataModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IdOfPostReply",
+                table: "ReportDataModels",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPost",
+                table: "ReportDataModels",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdOfPostReply",
+                table: "ReportDataModels");
+
+            migrationBuilder.DropColumn(
+                name: "IsPost",
+                table: "ReportDataModels");
+        }
+    }
+}
---
+++ Infrastructure/Data/Migrations/20250522070905_ReactionIds.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NoFilterForum.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReactionIds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReactionsPostRepliesIds",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "[]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReactionsPostRepliesIds",
+                table: "AspNetUsers");
+        }
+    }
+}
