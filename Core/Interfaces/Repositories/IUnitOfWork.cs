9023654c180c12f8b7bc4e97ecc40a8574e1b60e
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-26 11:02:15 +0300

    feat: Added UnitOfWork

---
+++ Core/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoFilterForum.Core.Interfaces.Repositories;
+
+namespace Core.Interfaces.Repositories
+{
+    public interface IUnitOfWork
+    {
+        IPostRepository Posts { get; }
+        IReplyRepository Replies { get; }
+        IUserRepository Users { get; }
+        ISectionRepository Sections { get; }
+        INotificationRepository Notifications { get; }
+        IReportRepository Reports { get; }
+        IWarningRepository Warnings { get; }
+        Task<int> CommitAsync();
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+    }
+}
---
+++ Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore.Storage;
+using NoFilterForum.Core.Interfaces.Repositories;
+using NoFilterForum.Infrastructure.Data;
+
+namespace Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+            private readonly ApplicationDbContext _context;
+            private IDbContextTransaction _transaction;
+
+            public UnitOfWork(ApplicationDbContext context,
+                              IPostRepository postRepository,
+                              IReplyRepository replyRepository,
+                              IUserRepository userRepository,
+                              INotificationRepository notificationRepository,
+                              IWarningRepository warningRepository,
+                              ISectionRepository sectionRepository,
+                              IReportRepository reportRepository)
+            {
+                _context = context;
+                Posts = postRepository;
+                Replies = replyRepository;
+                Users = userRepository;
+                Notifications = notificationRepository;
+                Sections = sectionRepository;
+                Reports = reportRepository;
+                Warnings= warningRepository;
+            }
+
+            public IPostRepository Posts { get; }
+            public IReplyRepository Replies { get; }
+            public IUserRepository Users { get; }
+            public IReportRepository Reports { get; }
+            public INotificationRepository Notifications { get; }
+            public ISectionRepository Sections { get; }
+            public IWarningRepository Warnings { get; }
+
+            public async Task<int> CommitAsync()
+            {
+                return await _context.SaveChangesAsync();
+            }
+
+            public async Task BeginTransactionAsync()
+            {
+                _transaction = await _context.Database.BeginTransactionAsync();
+            }
+
+            public async Task CommitTransactionAsync()
+            {
+                await _transaction.CommitAsync();
+            }
+
+            public async Task RollbackTransactionAsync()
+            {
+                await _transaction.RollbackAsync();
+            }
+        }
+}
