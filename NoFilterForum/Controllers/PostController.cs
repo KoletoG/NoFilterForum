fb5cec19a04654621043b458ab2ea414ef3dfbaa
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-27 10:33:26 +0300

    chore: Added more controllers

---
+++ NoFilterForum/Controllers/PostController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NoFilterForum.Core.Interfaces.Services;
+
+namespace Web.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IPostService _postService;
+        public PostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+    }
+}
