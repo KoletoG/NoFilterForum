ca2272c0cf6f758f9ea9094b4bdf3437c723c18f
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-28 10:50:27 +0300

    chore: Added NotificationsController

---
+++ NoFilterForum/Controllers/NotificationsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NoFilterForum.Core.Interfaces.Services;
+
+namespace Web.Controllers
+{
+    public class NotificationsController : Controller
+    {
+        private readonly INotificationService _notificationService;
+        public NotificationsController(INotificationService notificationService) 
+        {
+            _notificationService = notificationService;
+        }
+    }
+}
