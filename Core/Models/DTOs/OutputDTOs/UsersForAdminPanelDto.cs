c67e184b50dd4b6b222e78d8825eb60b2e10c32d
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-28 17:17:28 +0300

    chore: Added UserForAdminPanelDto()

---
+++ Core/Models/DTOs/OutputDTOs/UsersForAdminPanelDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Enums;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace Core.Models.DTOs.OutputDTOs
+{
+    public class UsersForAdminPanelDto
+    {
+        public string Email { get; set; }
+        public UserRoles Role { get; set; }
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public int WarningsCount { get; set; }
+
+    }
+}
