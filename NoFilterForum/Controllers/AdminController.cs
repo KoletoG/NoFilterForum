09383958637a35c6252259009918654b01e4bd32
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-29 08:23:01 +0300

    fix: Fixed rerouting from Admin to warnings

--- NoFilterForum/Controllers/AdminController.cs
+++ NoFilterForum/Controllers/AdminController.cs
@@ -191,23 +191,5 @@ namespace Web.Controllers
                 _ => Problem()
             }; 
         }
-        [Authorize]
-        [Route("WarningsOfUserId-{id}")]
-        [ResponseCache(Duration =60,Location =ResponseCacheLocation.Any)]
-        public async Task<IActionResult> ShowWarnings(string id)
-        {
-            if (!UserConstants.adminNames.Contains(User.Identity.Name))
-            {
-                return Forbid();
-            }
-            id = HttpUtility.UrlDecode(id);
-            if (string.IsNullOrEmpty(id))
-            {
-                return BadRequest("Id cannot be null");
-            }
-            var warningsDto = await _warningService.GetWarningsByUserIdAsync(id);
-            var warningsVM = warningsDto.Select(WarningMappers.MapToViewModel).ToList();
-            return View(warningsVM);
-        }
     }
 }
--- NoFilterForum/Controllers/WarningsController.cs
+++ NoFilterForum/Controllers/WarningsController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
 using NoFilterForum.Core.Interfaces.Services;
+using Web.Mappers.Warnings;
+using Core.Constants;
 
 namespace Web.Controllers
 {
@@ -13,6 +17,24 @@ namespace Web.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        [Authorize]
+        [HttpGet]
+        [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any)]
+        public async Task<IActionResult> Details(string id)
+        {
+            if (!UserConstants.adminNames.Contains(User.Identity.Name))
+            {
+                return Forbid();
+            }
+            id = HttpUtility.UrlDecode(id);
+            if (string.IsNullOrEmpty(id))
+            {
+                return BadRequest("Id cannot be null");
+            }
+            var warningsDto = await _warningService.GetWarningsByUserIdAsync(id);
+            var warningsVM = warningsDto.Select(WarningMappers.MapToViewModel).ToList();
+            return View(warningsVM);
         }
     }
 }
