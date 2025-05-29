cbd641f4cb32e1934fb5995342fe0a208462f1cf
KoletoG <twindestroyersbgbg@gmail.com>
2025-05-29 08:13:57 +0300

    feat: Created WarningsController

---
+++ NoFilterForum/Controllers/WarningsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NoFilterForum.Core.Interfaces.Services;
+
+namespace Web.Controllers
+{
+    public class WarningsController : Controller
+    {
+        private readonly IWarningService _warningService;
+        public WarningsController(IWarningService warningService)
+        {
+            _warningService = warningService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
