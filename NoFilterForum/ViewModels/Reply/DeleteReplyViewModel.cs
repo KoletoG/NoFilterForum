fc2345c3d5df39e333d890b1625a7e6f7c3257d8
Koleto <twindestroyersbgbg@gmail.com>
2025-05-30 17:58:39 +0300

    chore: Added attributes for validation in DeleteReplyViewModel

--- NoFilterForum/ViewModels/Reply/DeleteReplyViewModel.cs
+++ NoFilterForum/ViewModels/Reply/DeleteReplyViewModel.cs
@@ -1,9 +1,14 @@
-﻿namespace Web.ViewModels.Reply
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.ViewModels.Reply
 {
     public class DeleteReplyViewModel
     {
+        [Required]
         public string ReplyId { get; set; }
+        [Required]
         public string TitleOfSection { get; set; }
+        [Required]
         public string PostId { get; set; }
     }
 }
