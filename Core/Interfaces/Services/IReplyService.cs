64b5b99efb3fb149977cd4ca2bb192bf0be29867
Koleto <twindestroyersbgbg@gmail.com>
2025-05-30 17:52:09 +0300

    feat: Added DeleteReplyByIdAsync() in ReplyService

--- Core/Interfaces/Services/IReplyService.cs
+++ Core/Interfaces/Services/IReplyService.cs
@@ -1,8 +1,11 @@
-﻿using NoFilterForum.Core.Models.DataModels;
+﻿using Core.Enums;
+using Core.Models.DTOs.InputDTOs;
+using NoFilterForum.Core.Models.DataModels;
 
 namespace NoFilterForum.Core.Interfaces.Services
 {
     public interface IReplyService
     {
+        public Task<PostResult> DeleteReplyByIdAsync(DeleteReplyRequest request);
     }
 }
--- Infrastructure/Services/ReplyService.cs
+++ Infrastructure/Services/ReplyService.cs
@@ -2,6 +2,7 @@
 using Core.Enums;
 using Core.Interfaces.Repositories;
 using Core.Models.DTOs.InputDTOs;
+using Microsoft.Extensions.Logging;
 using NoFilterForum.Core.Interfaces.Repositories;
 using NoFilterForum.Core.Interfaces.Services;
 using NoFilterForum.Core.Models.DataModels;
@@ -12,13 +13,20 @@ namespace NoFilterForum.Infrastructure.Services
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IUserService _userService;
-        public ReplyService(IUnitOfWork unitOfWork, IUserService userService)
+        private readonly ILogger<ReplyService> _logger;
+        public ReplyService(IUnitOfWork unitOfWork, IUserService userService, ILogger<ReplyService> logger)
         {
             _unitOfWork = unitOfWork;
+            _logger = logger;
             _userService = userService;
         }
         public async Task<PostResult> DeleteReplyByIdAsync(DeleteReplyRequest request)
         {
+            var reply = await _unitOfWork.Replies.GetByIdAsync(request.ReplyId);
+            if(reply == null)
+            {
+                return PostResult.NotFound;
+            }
             string replyUserId = await _unitOfWork.Replies.GetUserIdByReplyIdAsync(request.ReplyId);
             if (replyUserId != request.UserId)
             {
@@ -34,8 +42,22 @@ namespace NoFilterForum.Infrastructure.Services
                 userOfReply.DecrementPostCount();
             }
             var notifications = await _unitOfWork.Notifications.GetAllByReplyIdAsync(request.ReplyId);
-            
-            return PostResult.Success;
+            try
+            {
+                await _unitOfWork.BeginTransactionAsync();
+                await _unitOfWork.Notifications.DeleteRangeAsync(notifications);
+                await _unitOfWork.Users.UpdateAsync(userOfReply);
+                await _unitOfWork.Replies.DeleteAsync(reply);
+                await _unitOfWork.CommitAsync();
+                await _unitOfWork.CommitTransactionAsync();
+                return PostResult.Success;
+            }
+            catch (Exception ex) 
+            {
+                await _unitOfWork.RollbackTransactionAsync();
+                _logger.LogError(ex, "Reply with Id: {ReplyId} wasn't deleted.", request.ReplyId);
+                return PostResult.UpdateFailed;
+            }
         }
     }
 }
