d768b51e9db0b8475c159500304b412809ed5a61
Koleto <twindestroyersbgbg@gmail.com>
2025-05-30 16:12:11 +0300

    feat: Added DeleteReplyViewModel

--- NoFilterForum/Controllers/ReplyController.cs
+++ NoFilterForum/Controllers/ReplyController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NoFilterForum.Core.Interfaces.Services;
 
 namespace Web.Controllers
 {
     public class ReplyController : Controller
     {
+        private readonly IReplyService _replyService;
+        public ReplyController(IReplyService replyService) 
+        { 
+            _replyService = replyService;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        public async Task<IActionResult> Delete(string id)
+        {
+
+            return Ok();
         }
     }
 }
---
+++ NoFilterForum/ViewModels/Reply/DeleteReplyViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.ViewModels.Reply
+{
+    public class DeleteReplyViewModel
+    {
+        public string ReplyId { get; set; }
+        public string TitleOfSection { get; set; }
+    }
+}
