714e4942136942cd24bed9de2bb6a68c3f052528
KoletoG <twindestroyersbgbg@gmail.com>
2025-06-01 09:39:24 +0300

    feat: Added SectionController

---
+++ NoFilterForum/Controllers/SectionController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NoFilterForum.Core.Interfaces.Services;
+
+namespace Web.Controllers
+{
+    public class SectionController : Controller
+    {
+        private readonly ISectionService _sectionService;
+        public SectionController(ISectionService sectionService) 
+        {
+            _sectionService = sectionService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
