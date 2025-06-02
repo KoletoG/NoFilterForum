8482bf298985e55edba601e2a8528dccafad1c0a
KoletoG <twindestroyersbgbg@gmail.com>
2025-06-02 18:47:19 +0300

    feat: Added PostFactory

---
+++ Core/Interfaces/Factories/IPostFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoFilterForum.Core.Models.DataModels;
+
+namespace Core.Interfaces.Factories
+{
+    public interface IPostFactory
+    {
+        public PostDataModel Create(string title, string body, UserDataModel user);
+    }
+}
---
+++ Infrastructure/Factories/PostFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces.Factories;
+using Core.Utility;
+using Ganss.Xss;
+using NoFilterForum.Core.Models.DataModels;
+
+namespace Infrastructure.Factories
+{
+    public class PostFactory : IPostFactory
+    {
+        private readonly IHtmlSanitizer _htmlSanitizer;
+        public PostFactory(IHtmlSanitizer htmlSanitizer)
+        {
+            _htmlSanitizer = htmlSanitizer;
+        }
+        public PostDataModel Create(string title, string body, UserDataModel user)
+        {
+            string sanitizedFormattedBody = TextFormatter.FormatBody(_htmlSanitizer.Sanitize(body));
+            string sanitizedTitle = _htmlSanitizer.Sanitize(title);
+            return new(sanitizedTitle, sanitizedFormattedBody,user);
+        }
+    }
+}
