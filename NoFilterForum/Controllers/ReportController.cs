87691b71bd5568843251623547fd1feabfe2648f
KoletoG <twindestroyersbgbg@gmail.com>
2025-06-05 18:33:36 +0300

    feat: Added ReportController

---
+++ NoFilterForum/Controllers/ReportController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NoFilterForum.Core.Interfaces.Services;
+
+namespace Web.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly IUserService _userService;
+        private readonly IReportService _reportService;
+        public ReportController(IUserService userService, IReportService reportService)
+        {
+            _userService = userService;
+            _reportService = reportService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
