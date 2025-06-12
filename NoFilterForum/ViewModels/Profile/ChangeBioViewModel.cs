573499580e21cd4931b8137a2b931b1163a7d2d5
KoletoG <twindestroyersbgbg@gmail.com>
2025-06-12 17:28:34 +0300

    refactor: ChangeBio has been moved to a separate view

--- NoFilterForum/ViewModels/Profile/ChangeBioViewModel.cs
+++ NoFilterForum/ViewModels/Profile/ChangeBioViewModel.cs
@@ -5,6 +5,7 @@ namespace Web.ViewModels.Profile
     public class ChangeBioViewModel
     {
         [Required]
+        [MaxLength(170,ErrorMessage ="Bio cannot be longer than 170 characters")]
         public string Bio {  get; set; }
     }
 }
