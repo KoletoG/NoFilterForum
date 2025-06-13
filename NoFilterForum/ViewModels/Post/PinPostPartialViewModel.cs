f19575a1163956f594d01e014f797cc6261c1eaf
KoletoG <twindestroyersbgbg@gmail.com>
2025-06-13 14:38:33 +0300

    feat: Added PinPostPartialViewModel and _PinPost partial view

---
+++ NoFilterForum/ViewModels/Post/PinPostPartialViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Web.ViewModels.Admin;
+
+namespace Web.ViewModels.Post
+{
+    public class PinPostPartialViewModel
+    {
+        public PinPostViewModel PinPostVM { get; }
+        public bool IsPinned { get; private init; }
+        public PinPostPartialViewModel(bool isPinned, string postId)
+        {
+            IsPinned = isPinned;
+            PinPostVM = new PinPostViewModel()
+            {
+                PostId = postId
+            };
+        }
+    }
+}
