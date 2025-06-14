e152c278e5297e3439fe8136aaf5d7790cd645e5
KoletoG <twindestroyersbgbg@gmail.com>
2025-06-14 08:23:14 +0300

    feat: Added LikeDislikeViewModel

---
+++ NoFilterForum/ViewModels/LikeDislikeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Web.ViewModels.Post;
+using Web.ViewModels.Reply;
+
+namespace Web.ViewModels
+{
+    public class LikeDislikeViewModel
+    {
+        public LikeDislikePostViewModel PostLikeDislike { get; set; } = new LikeDislikePostViewModel();
+        public LikeDislikeReplyViewModel ReplyLikeDislike { get; set; } = new LikeDislikeReplyViewModel();
+        public string Id { get; }
+        protected bool IsLike { get; }
+        protected bool IsPost { get; }
+        public LikeDislikeViewModel(string id, bool isPost, bool isLike)
+        {
+            Id = id;
+            IsLike = isLike;
+            IsPost = isPost;
+        }
+    }
+}
