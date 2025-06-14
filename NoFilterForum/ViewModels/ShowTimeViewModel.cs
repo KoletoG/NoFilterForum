0e6b4ad73cdc0e91ee0032dfa3a692d9def84de0
KoletoG <twindestroyersbgbg@gmail.com>
2025-06-14 07:54:11 +0300

    feat: Added ShowTimeViewModel

---
+++ NoFilterForum/ViewModels/ShowTimeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Web.ViewModels
+{
+    public class ShowTimeViewModel
+    {
+        public TimeSpan TimeDifference { get; }
+        public ShowTimeViewModel(DateTime timeOfObject)
+        {
+            TimeDifference = DateTime.UtcNow - timeOfObject;
+        }
+    }
+}
