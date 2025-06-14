b38073eb8c5581dec926e98f6bea294776fe0a28
KoletoG <twindestroyersbgbg@gmail.com>
2025-06-14 08:11:21 +0300

    refactor: Moved business logic from _ShowTime to its ViewModel

--- NoFilterForum/ViewModels/ShowTimeViewModel.cs
+++ NoFilterForum/ViewModels/ShowTimeViewModel.cs
@@ -2,10 +2,33 @@
 {
     public class ShowTimeViewModel
     {
-        public TimeSpan TimeDifference { get; }
+        public string DisplayTimeAgo { get; private set; }
+
         public ShowTimeViewModel(DateTime timeOfObject)
         {
-            TimeDifference = DateTime.UtcNow - timeOfObject;
+            TimeSpan timeDifference = DateTime.UtcNow - timeOfObject;
+            
+            CalculateDisplayTimeAgo(timeDifference);
+        }
+        private void CalculateDisplayTimeAgo(TimeSpan timediff)
+        {
+            double totalSeconds = timediff.TotalSeconds;
+            double totalMinutes = timediff.TotalMinutes;
+            double totalHours = timediff.TotalHours;
+            double totalDays = timediff.TotalDays;
+
+            if (totalSeconds < 60)
+                DisplayTimeAgo = $"{totalSeconds:0} seconds ago";
+            else if (totalMinutes < 60)
+                DisplayTimeAgo = $"{totalMinutes:0} minutes ago";
+            else if (totalHours < 24)
+                DisplayTimeAgo = $"{totalHours:0} hours ago";
+            else if (totalDays < 31)
+                DisplayTimeAgo = $"{totalDays:0} days ago";
+            else if (totalDays < 365)
+                DisplayTimeAgo = $"{(totalDays / 30):0} months ago";
+            else
+                DisplayTimeAgo = $"{(totalDays / 365):0} years ago";
         }
     }
 }
