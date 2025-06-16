4e9d2d6f4faf27e881dcfbf2e792b5f1458680af
Koleto <twindestroyersbgbg@gmail.com>
2025-06-16 15:49:05 +0300

    chore: PagingViewModel is now completed

--- NoFilterForum/ViewModels/PagingViewModel.cs
+++ NoFilterForum/ViewModels/PagingViewModel.cs
@@ -2,5 +2,16 @@
 {
     public class PagingViewModel
     {
+        public int Page {  get; private init; }
+        public int TotalPages { get; private init; }
+        public string ControllerName { get; private init; }
+        public string ActionName { get; private init; }
+        public PagingViewModel(int page, int totalPages, string controllerName, string actionName)
+        {
+            Page = page;
+            TotalPages = totalPages;
+            ControllerName = controllerName;
+            ActionName = actionName;
+        }
     }
 }
