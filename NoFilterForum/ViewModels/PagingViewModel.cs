57e3adff21c8a8ab59ca6065d942857a68acdf1f
Koleto <twindestroyersbgbg@gmail.com>
2025-06-16 15:53:06 +0300

    feat: _Paging is now completed

--- NoFilterForum/ViewModels/PagingViewModel.cs
+++ NoFilterForum/ViewModels/PagingViewModel.cs
@@ -1,17 +1,29 @@
-﻿namespace Web.ViewModels
+﻿using Microsoft.Extensions.Primitives;
+
+namespace Web.ViewModels
 {
     public class PagingViewModel
     {
-        public int Page {  get; private init; }
+        public int Page { get; private init; }
         public int TotalPages { get; private init; }
         public string ControllerName { get; private init; }
         public string ActionName { get; private init; }
-        public PagingViewModel(int page, int totalPages, string controllerName, string actionName)
+        public string TitleOfSection { get; private init; } = string.Empty;
+
+        public bool HasPrevious => Page > 1;
+        public bool HasNext => Page < TotalPages;
+
+        public int PreviousPage => Page - 1;
+        public int NextPage => Page + 1;
+
+        public PagingViewModel(int page, int totalPages, string controllerName, string actionName, string titleOfSection = "")
         {
             Page = page;
             TotalPages = totalPages;
             ControllerName = controllerName;
             ActionName = actionName;
+            TitleOfSection = titleOfSection;
         }
     }
+
 }
