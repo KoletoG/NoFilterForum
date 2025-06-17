4685f2c6264d3b98f4883bd2e097c88c597da19e
KoletoG <twindestroyersbgbg@gmail.com>
2025-06-17 22:41:04 +0300

    refactor: _RoleColor now accepts UserId as a parameter

--- NoFilterForum/ViewModels/RoleViewModel.cs
+++ NoFilterForum/ViewModels/RoleViewModel.cs
@@ -6,18 +6,15 @@ namespace Web.ViewModels
     public class RoleViewModel
     {
         public UserRoles Role {  get; set; }
-        public string Username { get; set; }
+        public string UserId { get; set; }
         public bool ShouldRoute {  get; set; }
-        public string EncodedUsername { get; private init; }
-        public RoleViewModel(UserRoles role, string username, bool shouldRoute)
+        public string Username { get; set; }
+        public RoleViewModel(UserRoles role, string userId,string username, bool shouldRoute)
         {
             Role = role;
+            UserId = userId;
             Username = username;
             ShouldRoute = shouldRoute;
-            if (ShouldRoute)
-            {
-                EncodedUsername = HttpUtility.UrlEncode(Username);
-            }
         }
     }
 }
