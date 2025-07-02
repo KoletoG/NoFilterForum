cb1be36e1683739e83138dda0bd9cc74989b4a35
Koleto <twindestroyersbgbg@gmail.com>
2025-07-02 22:27:08 +0300

    feat: Added TogglePin for Post Test

---
+++ UnitTests/PostTests.cs
@@ -0,0 +1,22 @@
+﻿using NoFilterForum.Core.Models.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class PostTests
+    {
+        [Fact]
+        public void TogglePin_ShouldInvertIsPinned() {
+            var post = new PostDataModel();
+            Assert.False(post.IsPinned);
+            post.TogglePin();
+            Assert.True(post.IsPinned);
+            post.TogglePin();
+            Assert.False(post.IsPinned);
+        }
+    }
+}
