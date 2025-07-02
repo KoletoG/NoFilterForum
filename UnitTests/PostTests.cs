1fdb4034637ec9fa9cd7f3a9110c291f46032504
Koleto <twindestroyersbgbg@gmail.com>
2025-07-02 22:32:48 +0300

    feat: Added IncrementLikes for Post test

--- UnitTests/PostTests.cs
+++ UnitTests/PostTests.cs
@@ -18,5 +18,15 @@ namespace UnitTests
             post.TogglePin();
             Assert.False(post.IsPinned);
         }
+        [Fact]
+        public void IncrementLikes_ShouldIncrementLikesByOne()
+        {
+            var post = new PostDataModel();
+            Assert.Equal(0,post.Likes);
+            post.IncrementLikes();
+            Assert.Equal(1,post.Likes);
+            post.IncrementLikes();
+            Assert.Equal(2,post.Likes);
+        }
     }
 }
