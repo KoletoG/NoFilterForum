32c915e6638c49dafd734942f2eaa65054e38d45
Koleto <twindestroyersbgbg@gmail.com>
2025-07-03 21:30:15 +0300

    feat: Added ReplyTests and test for IncrementLikes()

---
+++ UnitTests/ReplyTests.cs
@@ -0,0 +1,23 @@
+﻿using NoFilterForum.Core.Models.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class ReplyTests
+    {
+        [Fact]
+        public void IncrementLikes_ShouldAddOneToLikes()
+        {
+            var reply = new ReplyDataModel();
+            Assert.Equal(0, reply.Likes);
+            reply.IncrementLikes();
+            Assert.Equal(1, reply.Likes);
+            reply.IncrementLikes();
+            Assert.Equal(2,reply.Likes);
+        }
+    }
+}
