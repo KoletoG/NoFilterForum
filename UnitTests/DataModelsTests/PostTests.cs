a2b73f2189b08b875fa53c394bd6466ce9dfadd6
Koleto <twindestroyersbgbg@gmail.com>
2025-07-03 21:46:54 +0300

    feat: Added WarningTests
    Created a folder DataModelsTests

--- UnitTests/DataModelsTests/PostTests.cs
+++ UnitTests/DataModelsTests/PostTests.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UnitTests
+namespace UnitTests.DataModelsTests
 {
     public class PostTests
     {
--- UnitTests/DataModelsTests/ReplyTests.cs
+++ UnitTests/DataModelsTests/ReplyTests.cs
@@ -7,7 +7,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UnitTests
+namespace UnitTests.DataModelsTests
 {
     public class ReplyTests
     {
