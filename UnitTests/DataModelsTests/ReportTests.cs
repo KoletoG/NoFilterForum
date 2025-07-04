bdcf6ff9472740e78f60704e1cfc0cea56f9a1c2
Koleto <twindestroyersbgbg@gmail.com>
2025-07-04 15:55:14 +0300

    feat: Added Constructor test for Report

--- UnitTests/DataModelsTests/ReportTests.cs
+++ UnitTests/DataModelsTests/ReportTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NoFilterForum.Core.Models.DataModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,18 @@ namespace UnitTests.DataModelsTests
         [Fact]
         public void Constructor_ShouldAssignProperValues()
         {
-
+            var userTo = new UserDataModel();
+            var content = "Test content";
+            var idOfPostReply = "Test Id";
+            bool isPost = false;
+            var userFrom = new UserDataModel();
+            var report = new ReportDataModel(userTo,content,idOfPostReply,isPost,userFrom);
+            Assert.Equal(userTo, report.UserTo);
+            Assert.Equal(content, report.Content);
+            Assert.Equal(userFrom, report.UserFrom);
+            Assert.Equal(isPost, report.IsPost);
+            Assert.Equal(idOfPostReply, report.IdOfPostReply);
+            Assert.False(string.IsNullOrEmpty(report.Id));
         }
     }
 }
