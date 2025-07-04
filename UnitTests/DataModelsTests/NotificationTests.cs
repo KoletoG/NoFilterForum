58c842501924b9049733024342e34c406623755c
Koleto <twindestroyersbgbg@gmail.com>
2025-07-04 15:24:02 +0300

    feat: Added constructor test for NotificationDataModel

--- UnitTests/DataModelsTests/NotificationTests.cs
+++ UnitTests/DataModelsTests/NotificationTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NoFilterForum.Core.Models.DataModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,17 @@ namespace UnitTests.DataModelsTests
 {
     public class NotificationTests
     {
+        [Fact]
+        public void Constructor_ShouldAssignCorrectValues()
+        {
+            var reply = new ReplyDataModel();
+            var userFrom = new UserDataModel();
+            var userTo = new UserDataModel();
+            var notification = new NotificationDataModel(reply,userFrom,userTo);
+            Assert.Equal(reply, notification.Reply);
+            Assert.Equal(userTo, notification.UserTo);
+            Assert.Equal(userFrom, notification.UserFrom);
+            Assert.False(string.IsNullOrEmpty(notification.Id));
+        }
     }
 }
