4e3ea7554dc51e4a903cd5250b885125d00297b8
Koleto <twindestroyersbgbg@gmail.com>
2025-07-07 21:10:40 +0300

    feat: Added Create test for SectionFactory

--- UnitTests/FactoryTests/SectionFactoryTests.cs
+++ UnitTests/FactoryTests/SectionFactoryTests.cs
@@ -1,5 +1,8 @@
-﻿using Ganss.Xss;
+﻿using AngleSharp;
+using Ganss.Xss;
+using Infrastructure.Factories;
 using Moq;
+using NoFilterForum.Core.Models.DataModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +17,16 @@ namespace UnitTests.FactoryTests
         public void Create_ShouldCreateSectionInstance()
         {
             var htmlSanitizerMock = new Mock<IHtmlSanitizer>();
-
+            htmlSanitizerMock.SetupGet(x => x.AllowedTags).Returns(new HashSet<string>());
+            htmlSanitizerMock.Setup(x => x.Sanitize(It.IsAny<string>(),It.IsAny<string>(),It.IsAny<IMarkupFormatter>())).Returns((string input,string a, IMarkupFormatter markup) => input);
+            var sectionFactory =  new SectionFactory(htmlSanitizerMock.Object);
+            string title = "Test title";
+            string description = "Test description";
+            var section = sectionFactory.Create(title, description);
+            Assert.NotNull(section);
+            Assert.IsType<SectionDataModel>(section);
+            Assert.Equal(title, section.Title);
+            Assert.Equal(description, section.Description);
         }
     }
 }
