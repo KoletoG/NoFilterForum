0cc24542f69d7485980bb962ebcea37e73e0292e
Koleto <twindestroyersbgbg@gmail.com>
2025-07-08 23:08:00 +0300

    refactor: Changed naming of 2 unit tests

--- UnitTests/Utility/PageUtilityTests.cs
+++ UnitTests/Utility/PageUtilityTests.cs
@@ -10,16 +10,16 @@ namespace UnitTests.Utility
     public class PageUtilityTests
     {
         [Fact]
-        public void ValidatePageNumber_WhenPageIsLessThanOne_ReturnsOne()
+        public void ValidatePageNumber_ShouldReturnOne_WhenPageIsLessThanOne()
         {
             int totalPages = 10;
-            var result1 = PageUtility.ValidatePageNumber(-1, totalPages);
-            var result2 = PageUtility.ValidatePageNumber(-5, totalPages);
+            var result1 = PageUtility.ValidatePageNumber(0, totalPages);
+            var result2 = PageUtility.ValidatePageNumber(-1, totalPages);
             Assert.Equal(1, result1);
             Assert.Equal(1, result2);
         }
         [Fact]
-        public void ValidatePageNumber_WhenTotalPagesAreOne_ReturnsOne()
+        public void ValidatePageNumber_ShouldReturnOne_WhenTotalPagesAreOne()
         {
             int totalPages = 1;
             int page = -5;
