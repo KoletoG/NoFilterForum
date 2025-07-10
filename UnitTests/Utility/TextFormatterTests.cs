de4c2182f1c981d9f6d8fdc6bb2b5509cffb7010
Koleto <twindestroyersbgbg@gmail.com>
2025-07-10 17:50:44 +0300

    feat: Added ReplaceLinkText test

--- UnitTests/Utility/TextFormatterTests.cs
+++ UnitTests/Utility/TextFormatterTests.cs
@@ -16,5 +16,13 @@ namespace UnitTests.Utility
             var result = TextFormatter.LinkCheckText(link);
             Assert.Equal("<a href=\"https://www.randomlink.com/\" target=\"_blank\" rel=\"noopener noreferrer nofollow\">https://www.randomlink.com/</a>", result);
         }
+        [Fact]
+        public void ReplaceLinkText()
+        {
+            string link = "<a href=\"https://www.randomlink.com/\" target=\"_blank\" rel=\"noopener noreferrer nofollow\">https://www.randomlink.com/</a>";
+            var result = TextFormatter.ReplaceLinkText(link);
+            Assert.Equal("https://www.randomlink.com/", result);
+
+        }
     }
 }
