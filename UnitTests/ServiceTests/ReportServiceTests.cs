4ebda0428ac9999c3f9a6247275514c04611941a
KoletoG <twindestroyersbgbg@gmail.com>
2025-07-12 15:35:41 +0300

    feat: Added GetAllReportsAsync test

--- UnitTests/ServiceTests/ReportServiceTests.cs
+++ UnitTests/ServiceTests/ReportServiceTests.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Castle.Core.Logging;
+using Core.Interfaces.Factories;
+using Core.Interfaces.Repositories;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NoFilterForum.Core.Models.DataModels;
+using NoFilterForum.Infrastructure.Services;
 
 namespace UnitTests.ServiceTests
 {
     public class ReportServiceTests
     {
+        [Fact]
+        public async Task GetAllReportsAsync_ShouldReturnEmptyListOfReports_WhenCalled()
+        {
+            var iUnitOfWorkMock = new Mock<IUnitOfWork>();
+            var reportFactoryMock = new Mock<IReportFactory>();
+            var iLoggerMock = new Mock<ILogger<ReportService>>();
+            iUnitOfWorkMock.Setup(x => x.Reports.GetAllAsync()).ReturnsAsync(new List<ReportDataModel>());
+            var reportService = new ReportService(iUnitOfWorkMock.Object,reportFactoryMock.Object,iLoggerMock.Object);
+            var result = await reportService.GetAllReportsAsync();
+            Assert.IsType<List<ReportDataModel>>(result);
+            Assert.NotNull(result);
+        }
     }
 }
