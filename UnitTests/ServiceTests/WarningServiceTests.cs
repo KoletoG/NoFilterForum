465c02e3f81891ca06013b5637a749d504a00b62
KoletoG <twindestroyersbgbg@gmail.com>
2025-07-15 15:52:06 +0300

    feat: Added AcceptWarningsAsync() test for when the list of warnings is empty or null

--- UnitTests/ServiceTests/WarningServiceTests.cs
+++ UnitTests/ServiceTests/WarningServiceTests.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Enums;
 using Core.Interfaces.Factories;
 using Core.Interfaces.Repositories;
 using Core.Models.DTOs.OutputDTOs.Warning;
 using Ganss.Xss;
 using Microsoft.Extensions.Logging;
 using Moq;
+using NoFilterForum.Core.Models.DataModels;
 using NoFilterForum.Infrastructure.Services;
 
 namespace UnitTests.ServiceTests
@@ -27,7 +29,19 @@ namespace UnitTests.ServiceTests
             var warningService = new WarningService(iUnitOfWorkMock.Object, iLoggerMock.Object, iHtmlSanitizerMock.Object);
             var result = await warningService.GetWarningsContentDtosByUserIdAsync(userId);
             Assert.IsType<List<WarningsContentDto>>(result);
-
+        }
+        [Fact]
+        public async Task AcceptWarningsAsync_ShouldReturnSuccess_WhenListWarningsIsEmpty()
+        {
+            string userId = "TestUserId";
+            var iHtmlSanitizerMock = new Mock<IHtmlSanitizer>();
+            var iUnitOfWorkMock = new Mock<IUnitOfWork>();
+            var iLoggerMock = new Mock<ILogger<WarningService>>();
+            iUnitOfWorkMock.Setup(x => x.Warnings.GetAllByUserIdAsync(It.IsAny<string>())).ReturnsAsync((List<WarningDataModel>?)null);
+            iHtmlSanitizerMock.SetupGet(x => x.AllowedTags).Returns(new HashSet<string>());
+            var warningService = new WarningService(iUnitOfWorkMock.Object, iLoggerMock.Object, iHtmlSanitizerMock.Object);
+            var result = await warningService.AcceptWarningsAsync(userId);
+            Assert.Equal(PostResult.Success, result);
         }
     }
 }
