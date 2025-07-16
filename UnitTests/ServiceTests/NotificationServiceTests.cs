b02800a889d76ef6916096eaa99388e288ac85a0
KoletoG <twindestroyersbgbg@gmail.com>
2025-07-16 12:02:01 +0300

    feat: Added GetNotificationDtosByUserIdAsync() test when userId is valid

--- UnitTests/ServiceTests/NotificationServiceTests.cs
+++ UnitTests/ServiceTests/NotificationServiceTests.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Interfaces.Repositories;
+using Core.Models.DTOs.OutputDTOs.Notification;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NoFilterForum.Infrastructure.Services;
 
 namespace UnitTests.ServiceTests
 {
     public class NotificationServiceTests
     {
-
+        [Fact]
+        public async Task GetNotificationsDtosByUserIdAsync_ShouldReturnListOfNotificationDtos_WhenUserIdIsValid()
+        {
+            var iUnitOfWorkMock = new Mock<IUnitOfWork>();
+            var iLoggerMock = new Mock<ILogger<NotificationService>>();
+            var userId = "TestUserId";
+            iUnitOfWorkMock.Setup(x => x.Notifications.GetNotificationsAsDtoByUserIdAsync(It.IsAny<string>())).ReturnsAsync(new List<NotificationsDto>());
+            var notificationService = new NotificationService(iUnitOfWorkMock.Object,iLoggerMock.Object);
+            var result = await notificationService.GetNotificationsDtosByUserIdAsync(userId);
+            Assert.NotNull(result);
+            Assert.IsType<List<NotificationsDto>>(result);
+        }
     }
 }
