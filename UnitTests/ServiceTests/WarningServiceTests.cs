b22792f6a11751099a44832c919947cec70ae713
KoletoG <twindestroyersbgbg@gmail.com>
2025-07-16 11:48:21 +0300

    feat: Added AddWarningAsync test for Success

--- UnitTests/ServiceTests/WarningServiceTests.cs
+++ UnitTests/ServiceTests/WarningServiceTests.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AngleSharp;
 using Core.Enums;
 using Core.Interfaces.Factories;
 using Core.Interfaces.Repositories;
+using Core.Models.DTOs.InputDTOs.Warning;
 using Core.Models.DTOs.OutputDTOs.Warning;
 using Ganss.Xss;
 using Microsoft.Extensions.Logging;
@@ -70,6 +72,20 @@ namespace UnitTests.ServiceTests
             var warningService = new WarningService(iUnitOfWorkMock.Object, iLoggerMock.Object, iHtmlSanitizerMock.Object);
             var result = await warningService.AcceptWarningsAsync(userId);
             Assert.Equal(PostResult.UpdateFailed, result);
+        }
+        [Fact]
+        public async Task AddWarningAsync_ShouldReturnSuccess_WhenRequestIsValid()
+        {
+            var createWarningRequest = new CreateWarningRequest() { Content = "TestContent", UserId = "TestUserId" };
+            var iHtmlSanitizerMock = new Mock<IHtmlSanitizer>();
+            var iUnitOfWorkMock = new Mock<IUnitOfWork>();
+            var iLoggerMock = new Mock<ILogger<WarningService>>();
+            iHtmlSanitizerMock.Setup(x=>x.Sanitize(It.IsAny<string>(),It.IsAny<string>(),It.IsAny<IMarkupFormatter>())).Returns((string input, string a, IMarkupFormatter b) => input);
+            iUnitOfWorkMock.Setup(x => x.Users.GetUserWithWarningsByIdAsync(It.IsAny<string>())).ReturnsAsync(new UserDataModel() { Warnings = new List<WarningDataModel>()});
+            iHtmlSanitizerMock.SetupGet(x => x.AllowedTags).Returns(new HashSet<string>());
+            var warningService = new WarningService(iUnitOfWorkMock.Object, iLoggerMock.Object, iHtmlSanitizerMock.Object);
+            var result = await warningService.AddWarningAsync(createWarningRequest);
+            Assert.Equal(PostResult.Success, result);
         }
     }
 }
