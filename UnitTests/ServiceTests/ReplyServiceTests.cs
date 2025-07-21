60266dd5b9177a43599487a17a35237b9d785331
KoletoG <twindestroyersbgbg@gmail.com>
2025-07-21 17:24:44 +0300

    feat: Added GetListReplyItemDtoAsync() test for valid request

--- UnitTests/ServiceTests/ReplyServiceTests.cs
+++ UnitTests/ServiceTests/ReplyServiceTests.cs
@@ -17,13 +17,6 @@ namespace UnitTests.ServiceTests
 {
     public class ReplyServiceTests
     {
-        /*
-         
-
-
-
-        
-         */
         [Fact]
         public async Task GetListReplyIndexItemDto_ShouldReturnListReplyIndexItemDto_WhenRequestIsValid()
         {
@@ -46,6 +39,29 @@ namespace UnitTests.ServiceTests
             var result = await replyService.GetListReplyIndexItemDto(getListReplyIndexItemRequest);
             Assert.NotNull(result);
             Assert.IsType<List<ReplyIndexItemDto>>(result);
+        }
+
+        [Fact]
+        public async Task GetListReplyItemDtoAsync_ShouldReturnListReplyItemDto_WhenRequestIsValid()
+        {
+            Mock<IUnitOfWork> unitOfWorkMock = new();
+            Mock<IUserService> userServiceMock = new();
+            Mock<ILogger<ReplyService>> loggerMock = new();
+            Mock<IHtmlSanitizer> htmlSanitizerMock = new();
+            Mock<IReplyFactory> replyFactoryMock = new();
+            htmlSanitizerMock.SetupGet(x => x.AllowedTags).Returns(new HashSet<string>());
+            unitOfWorkMock.Setup(x => x.Replies.GetListReplyItemDtoByUserIdAsync(It.IsAny<string>())).ReturnsAsync(new List<ReplyItemDto>());
+            var getReplyItemRequest = new GetReplyItemRequest() { UserId = "UserIdExample" };
+            var replyService = new ReplyService(
+                unitOfWorkMock.Object,
+                replyFactoryMock.Object,
+                userServiceMock.Object,
+                loggerMock.Object,
+                htmlSanitizerMock.Object
+                );
+            var result = await replyService.GetListReplyItemDtoAsync(getReplyItemRequest);
+            Assert.IsType<List<ReplyItemDto>>(result);
+            Assert.NotNull(result);
         }
     }
 }
