9d85ed0b8dfaa90fde14bcc3bf89ed7491ecb9a8
KoletoG <twindestroyersbgbg@gmail.com>
2025-07-21 17:19:44 +0300

    feat: Added GetListReplyIndexItemDto() test for when request is valid

--- UnitTests/ServiceTests/ReplyServiceTests.cs
+++ UnitTests/ServiceTests/ReplyServiceTests.cs
@@ -3,11 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Interfaces.Factories;
+using Core.Interfaces.Repositories;
+using Core.Models.DTOs.InputDTOs.Reply;
+using Core.Models.DTOs.OutputDTOs.Reply;
+using Ganss.Xss;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NoFilterForum.Core.Interfaces.Services;
+using NoFilterForum.Infrastructure.Services;
 
 namespace UnitTests.ServiceTests
 {
     public class ReplyServiceTests
     {
+        /*
+         
 
+
+
+        
+         */
+        [Fact]
+        public async Task GetListReplyIndexItemDto_ShouldReturnListReplyIndexItemDto_WhenRequestIsValid()
+        {
+            Mock<IUnitOfWork> unitOfWorkMock = new();
+            Mock<IUserService> userServiceMock = new();
+            Mock<ILogger<ReplyService>> loggerMock = new();
+            Mock<IHtmlSanitizer> htmlSanitizerMock = new();
+            Mock<IReplyFactory> replyFactoryMock = new(); 
+            unitOfWorkMock.Setup(x => x.Replies.GetCountByPostIdAsync(It.IsAny<string>())).ReturnsAsync(5);
+            unitOfWorkMock.Setup(x => x.Replies.GetReplyIndexItemDtoListByPostIdAndPageAsync(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(new List<ReplyIndexItemDto>());
+            htmlSanitizerMock.SetupGet(x => x.AllowedTags).Returns(new HashSet<string>());
+            var replyService = new ReplyService(
+                unitOfWorkMock.Object,
+                replyFactoryMock.Object,
+                userServiceMock.Object,
+                loggerMock.Object,
+                htmlSanitizerMock.Object
+                );
+            var getListReplyIndexItemRequest = new GetListReplyIndexItemRequest() { Page = 1,PostId="ExampleId" };
+            var result = await replyService.GetListReplyIndexItemDto(getListReplyIndexItemRequest);
+            Assert.NotNull(result);
+            Assert.IsType<List<ReplyIndexItemDto>>(result);
+        }
     }
 }
