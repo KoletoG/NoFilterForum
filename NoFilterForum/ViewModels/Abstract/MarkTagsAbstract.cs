8e7d033d12e37de2223e9f0639101bccde00b6d2
KoletoG <twindestroyersbgbg@gmail.com>
2025-07-26 16:52:17 +0300

    chore: Changed names of methods in MarkTagsAbstract

--- NoFilterForum/ViewModels/Abstract/MarkTagsAbstract.cs
+++ NoFilterForum/ViewModels/Abstract/MarkTagsAbstract.cs
@@ -5,13 +5,13 @@ namespace Web.ViewModels.Abstract
     public abstract class MarkTagsAbstract
     {
         public string Content { get; set; }
-        private string MarkTags(string text, string username)
+        private string ReplaceTags(string text, string username)
         {
             return Regex.Replace(text, $@"(@{username})", "<span style=\"background-color: #e0e0e0;\">$1</span>");
         }
-        public void MarkTagsOfContent(string currentUsername)
+        public void MarkTags(string currentUsername)
         {
-            Content = string.Join(" ", Content.Split(' ').Select(x => MarkTags(x, currentUsername)));
+            Content = string.Join(" ", Content.Split(' ').Select(x => ReplaceTags(x, currentUsername)));
         }
     }
 }
