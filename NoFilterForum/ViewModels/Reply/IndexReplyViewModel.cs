4cab84c861381c07ec2aec9c643d7a62fcc1a010
KoletoG <twindestroyersbgbg@gmail.com>
2025-08-10 16:21:29 +0300

    feat: Reply number is now added in Reply Index View

--- NoFilterForum/ViewModels/Reply/IndexReplyViewModel.cs
+++ NoFilterForum/ViewModels/Reply/IndexReplyViewModel.cs
@@ -5,7 +5,7 @@
         public required string ReplyId { get;set; }
         public int Page {  get; set; }
         public int TotalPages { get; set; }
-        public required IReadOnlyCollection<ReplyIndexItemViewModel> Replies { get; set; }
+        public required IList<ReplyIndexItemViewModel> Replies { get; set; }
         public required PostReplyIndexViewModel Post {  get; set; }
         public required CurrentUserReplyIndexViewModel CurrentUser { get; set; }
     }
