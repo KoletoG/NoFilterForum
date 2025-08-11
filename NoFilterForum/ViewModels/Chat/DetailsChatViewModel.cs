71075d1c0cc0ab40d4adffa2dbe44b346df386a4
KoletoG <twindestroyersbgbg@gmail.com>
2025-08-11 18:40:20 +0300

    chore: Changed properties types

--- NoFilterForum/ViewModels/Chat/DetailsChatViewModel.cs
+++ NoFilterForum/ViewModels/Chat/DetailsChatViewModel.cs
@@ -4,7 +4,7 @@ namespace Web.ViewModels.Chat
 {
     public class DetailsChatViewModel
     {
-        public required IReadOnlyCollection<MessageDataModel> Messages { get; set; }
+        public required IEnumerable<MessageDataModel> Messages { get; set; }
         public required string Username1 { get; set; }
         public required string Username2 { get; set; }
     }
