fc4db836100f385d613c2766a4078dfb86ee527e
KoletoG <twindestroyersbgbg@gmail.com>
2025-08-13 08:17:36 +0300

    chore: Moved script to a separate file

---
+++ NoFilterForum/wwwroot/js/JSMessage.js
@@ -0,0 +1,22 @@
+﻿var form = document.getElementById('messageForm');
+form.addEventListener('submit',(event)=>{
+	event.preventDefault();
+});
+function submitMessage()
+{
+	let formData = new FormData(form);
+	fetch('/Message/Create',{
+		method: 'POST',
+		body: formData
+		}).then(response=>{
+		if(!response.ok)
+		{
+			throw new Error("Error has occcured");
+		}
+		return response.text();
+		}).then(data=>{
+		var label = document.createElement('label');
+		label.textContent=data;
+		document.appendChild(label);
+		});
+}
