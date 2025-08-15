7d5fef45c34fa3b56c30f79fa19bbfc6baf2b9c8
KoletoG <twindestroyersbgbg@gmail.com>
2025-08-15 17:26:26 +0300

    refactor: _RoleColor is now more maintainable and readable

--- NoFilterForum/ViewModels/RoleViewModel.cs
+++ NoFilterForum/ViewModels/RoleViewModel.cs
@@ -1,22 +1,36 @@
 ﻿using System.Web;
+using Core.Constants;
 using Core.Enums;
 
 namespace Web.ViewModels
 {
     public class RoleViewModel
     {
-        public UserRoles Role {  get; set; }
         public string UserId { get; set; }
         public bool ShouldRoute {  get; set; }
         public string Username { get; set; }
         public string ImageUrl { get; set; }
+        public string? RoleColor { get; private set; }
         public RoleViewModel(UserRoles role, string userId,string username, bool shouldRoute, string imageUrl)
         {
-            Role = role;
             UserId = userId;
             Username = username;
             ShouldRoute = shouldRoute;
             ImageUrl = imageUrl;
+            RoleColor = SetRoleColor(role);
+        }
+        private string SetRoleColor(UserRoles role)
+        {
+            return role switch
+            {
+                UserRoles.Newbie => ColorConstants.TextNewbie,
+                UserRoles.Admin => ColorConstants.TextAdmin,
+                UserRoles.VIP => ColorConstants.TextVIP,
+                UserRoles.Dinosaur => ColorConstants.TextDinosaur,
+                UserRoles.Regular => ColorConstants.TextRegular,
+                UserRoles.Deleted => ColorConstants.TextDefault,
+                _ => ColorConstants.TextNewbie
+            };
         }
     }
 }
