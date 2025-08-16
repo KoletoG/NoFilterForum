bb46f1dad0751b3e0564b8492cac932cea7e7b60
KoletoG <twindestroyersbgbg@gmail.com>
2025-08-16 20:36:02 +0300

    refactor: Made Index of Reply Controller more readable

--- NoFilterForum/ViewModels/Reply/IndexReplyViewModel.cs
+++ NoFilterForum/ViewModels/Reply/IndexReplyViewModel.cs
@@ -1,4 +1,7 @@
-﻿namespace Web.ViewModels.Reply
+﻿using Web.Controllers;
+using Web.Static_variables;
+
+namespace Web.ViewModels.Reply
 {
     public class IndexReplyViewModel
     {
@@ -6,6 +9,10 @@
         public int Page {  get; set; }
         public int TotalPages { get; set; }
         public bool IsAdmin { get; set; }
+        string controllerName = "Reply";
+        string actionName = "Index";
+        public string ReplyControllerName = ControllerNames.ReplyControllerName;
+        public string IndexName = nameof(ReplyController.Index)[..^5];
         public required IList<ReplyIndexItemViewModel> Replies { get; set; }
         public required PostReplyIndexViewModel Post {  get; set; }
         public required CurrentUserReplyIndexViewModel CurrentUser { get; set; }
