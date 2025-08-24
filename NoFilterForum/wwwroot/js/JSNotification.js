323b71ee6dfcc5c4a3eed841f80d6f9e487024d4
KoletoG <twindestroyersbgbg@gmail.com>
2025-08-24 10:39:21 +0300

    style: Notifications count now updates on every live event

--- NoFilterForum/wwwroot/js/JSNotification.js
+++ NoFilterForum/wwwroot/js/JSNotification.js
@@ -1,9 +1,9 @@
 ﻿const connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+let countNots=0;
 connection.start();
 connection.on("ReceiveNotification",()=>{
-let notification = document.getElementById('notificationLink');
-notification.classList.remove('text-dark');
-notification.classList.add('text-danger');
+    countNots++;
+    setNotification(countNots);
 });
 
 document.addEventListener('DOMContentLoaded',(e)=>{
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded',(e)=>{
     .then(response => response.text())
     .then(data => {
         setNotification(data);
+        countNots+=data;
     });
 });
 function setNotification(count){
