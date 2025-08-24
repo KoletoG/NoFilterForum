feae623436a8e4529644716e8bbdf6b1afd016de
KoletoG <twindestroyersbgbg@gmail.com>
2025-08-24 10:33:43 +0300

    feat: When a user logs in and has a notification, it shows in the layout

--- NoFilterForum/wwwroot/js/JSNotification.js
+++ NoFilterForum/wwwroot/js/JSNotification.js
@@ -1,9 +1,22 @@
 ﻿const connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 connection.start();
-
 connection.on("ReceiveNotification",()=>{
 let notification = document.getElementById('notificationLink');
 notification.classList.remove('text-dark');
 notification.classList.add('text-danger');
 });
 
+document.addEventListener('DOMContentLoaded',(e)=>{
+    fetch('/Notifications/GetNotifications')
+    .then(response => response.text())
+    .then(data => {
+        setNotification(data);
+    });
+});
+function setNotification(count){
+    if(count>0){
+let notification = document.getElementById('notificationLink');
+notification.classList.remove('text-dark');
+notification.classList.add('text-danger');
+    }
+}
