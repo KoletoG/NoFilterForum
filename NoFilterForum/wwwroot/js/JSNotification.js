61f56bff2dcf1112198244397f6689e07854b5e6
KoletoG <twindestroyersbgbg@gmail.com>
2025-08-24 10:20:09 +0300

    style: When a user receives a new notification, it instantly shows in notifications layout

---
+++ NoFilterForum/wwwroot/js/JSNotification.js
@@ -0,0 +1,9 @@
+﻿const connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+connection.start();
+
+connection.on("ReceiveNotification",()=>{
+let notification = document.getElementById('notificationLink');
+notification.classList.remove('text-dark');
+notification.classList.add('text-danger');
+});
+
