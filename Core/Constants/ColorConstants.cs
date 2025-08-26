90c97812f7a4b004b5dff5d381d640b0f8d40409
KoletoG <twindestroyersbgbg@gmail.com>
2025-08-26 10:49:44 +0300

    chore: Changed color of constants

--- Core/Constants/ColorConstants.cs
+++ Core/Constants/ColorConstants.cs
@@ -8,13 +8,13 @@ namespace Core.Constants
 {
     public static class ColorConstants
     {
-        public static readonly string TextNewbie = "text-black";
+        public static readonly string TextNewbie = "mainText";
         public static readonly string TextAdmin = "text-danger";
         public static readonly string TextVIP = "text-warning";
         public static readonly string TextDinosaur = "text-primary";
         public static readonly string TextRegular = "text-success";
         public static readonly string TextDefault = "text-black-50";
         public static readonly string TextLikeDislike = "mainText";
-        public static readonly string TextNoLikeDislike = "secondaryText";
+        public static readonly string TextNoLikeDislike = "mainText";
     }
 }
