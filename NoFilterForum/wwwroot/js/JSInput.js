f96e3394e438bc5478a6c2c878afc71a064d4f75
KoletoG <twindestroyersbgbg@gmail.com>
2025-08-30 15:26:19 +0300

    fix: Fixed special input not working when input already has text

--- NoFilterForum/wwwroot/js/JSInput.js
+++ NoFilterForum/wwwroot/js/JSInput.js
@@ -1,20 +1,20 @@
 ﻿function addBoldText(){
     let textInput = document.getElementById('textInput');
-    textInput.innerHTML+='<b>BOLD TEXT</b>';
+    textInput.value+='<b>BOLD TEXT</b>';
 }function addItalicText(){
     let textInput = document.getElementById('textInput');
-    textInput.innerHTML+='<i>ITALIC TEXT</i>';
+    textInput.value+='<i>ITALIC TEXT</i>';
 }function addBulletPoint(){
     let textInput = document.getElementById('textInput');
-    textInput.innerHTML+='&bull;';
+    textInput.value+='&bull;';
 }
 function addSmallerText(){
     
     let textInput = document.getElementById('textInput');
-    textInput.innerHTML+='<span class="smallerText">SMALLER TEXT</span>';
+    textInput.value+='<span class="smallerText">SMALLER TEXT</span>';
 }
 function addBiggerText(){
     
     let textInput = document.getElementById('textInput');
-    textInput.innerHTML+='<span class="biggerText">BIGGER TEXT</span>';
+    textInput.value+='<span class="biggerText">BIGGER TEXT</span>';
 }
