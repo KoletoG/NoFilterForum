6a46dede2f3c5e8bacabf0a844b580058099e18b
KoletoG <twindestroyersbgbg@gmail.com>
2025-09-01 13:50:51 +0300

    perf: Optimized searching of seen posts

---
+++ NoFilterForum/wwwroot/js/JSMarkSeenPosts.js
@@ -0,0 +1,11 @@
+﻿function markSeenPost(postId)
+{
+    let wasSeen = localStorage.getItem(`${postId}_seen`);
+    if(wasSeen == "1")
+    {
+        let post = document.getElementById(`${postId}_title`);
+        post.classList.add('secondaryText');
+        post.style='font-style:italic';
+        post.classList.remove('mainText');
+    }
+}
--- NoFilterForum/wwwroot/js/JSPostSeen.js
+++ NoFilterForum/wwwroot/js/JSPostSeen.js
@@ -1,13 +1,6 @@
 ﻿function  updateSeenState(e,postId)
 {
     e.preventDefault();
- fetch('/Post/UpdateSeenState',{
-    method:'POST',
-    headers:{
-        'Content-Type':'application/json'
-    },
-    body:JSON.stringify(postId)
-    });
     localStorage.setItem(`${postId}_seen`,"1"); // set to 1 as it is a string and to not waste unnecessary space in localStorage
     window.location.href = `/Reply/Index?postId=${postId}`;
 }
