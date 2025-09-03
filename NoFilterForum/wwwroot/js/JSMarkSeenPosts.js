dd2acb5e58ce224aa7e11cd206128a7fedb0f29f
KoletoG <twindestroyersbgbg@gmail.com>
2025-09-03 15:45:16 +0300

    style: Updated seen UI

--- NoFilterForum/wwwroot/js/JSMarkSeenPosts.js
+++ NoFilterForum/wwwroot/js/JSMarkSeenPosts.js
@@ -3,13 +3,7 @@
     let wasSeen = localStorage.getItem(`${postId}_seen`);
     if(wasSeen == "1")
     {
-        let title = document.getElementById(`${postId}_title`);
-        let body = document.getElementById(`${postId}_body`);
-        title.classList.add('secondaryText');
-        title.style='font-style:italic';
-        title.classList.remove('mainText');
-        body.classList.add('fst-italic');
-        body.classList.remove('mainText');
-        body.classList.add('secondaryText');
+        let date = document.getElementById(`${postId}_date`);
+        date.innerHTML+=" / seen";
     }
 }
