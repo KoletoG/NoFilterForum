e3dd89bcfa27d238e8c6a33f7d8e2b413e6ad96a
KoletoG <twindestroyersbgbg@gmail.com>
2025-09-11 10:31:12 +0300

    style: Improved Reply Index UI

--- NoFilterForum/ViewModels/LikeDislikeViewModel.cs
+++ NoFilterForum/ViewModels/LikeDislikeViewModel.cs
@@ -11,7 +11,7 @@ namespace Web.ViewModels
         public bool IsLiked { get; }
         public bool IsPost {  get; }
         public string Style => ColorRed ? "color:#236A96" : string.Empty;
-        public string Label => IsLiked ? "Like" : "Dislike";
+        public string Label => IsLiked ? "L" : "D";
         public LikeDislikeViewModel(string id, bool isPost, bool isLike, bool isMarked)
         {
             Id = id;
